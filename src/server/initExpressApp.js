9c3331449135bf6414fe2b517362b310f92adb15
minu0210 <113428736+minu0210@users.noreply.github.com>
2024-01-05 08:47:47 +0900

    Feat: Set methodOverride, urlencoded, static

--- src/server/initExpressApp.js
+++ src/server/initExpressApp.js
@@ -1,11 +1,15 @@
 const session = require('express-session')
 const express = require('express')
+const methodOverride = require('method-override')
 
 module.exports = function (app) {
-  app.set('trust proxy', 1)
-  app.use(session({
-      secret: 'v;ni#$!ni!@fvA{()IA@',
-      cookie: { secure: false }
-  }))
-  app.use(express.json())
+    app.set('trust proxy', 1)
+    app.use(session({
+        secret: 'v;ni#$!ni!@fvA{()IA@',
+        cookie: { secure: false }
+    }))
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true }))
+    app.use(express.static('static'))
+    app.use(methodOverride('_method'))
 }
